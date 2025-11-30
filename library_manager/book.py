e943ed82766533f2a8a1dcceb15adf8586a09dab
Manan <manansingh1921@gmail.com>
2025-11-30 18:36:43 +0530

    implement Book class with issue/return methods

---
+++ library_manager/book.py
@@ -0,0 +1,30 @@
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.status = "available"
+    
+    def issue(self):
+        if self.status == "available":
+            self.status = "issued"
+            return True
+        return False
+    
+    def return_book(self):
+        self.status = "available"
+        return True
+    
+    def is_available(self):
+        return self.status == "available"
+    
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'author': self.author,
+            'isbn': self.isbn,
+            'status': self.status
+        }
+    
+    def __str__(self):
+        return f"{self.title} by {self.author} [{self.isbn}] - {self.status}"
